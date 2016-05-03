b0ba0c02152fcb32a6dc45be1c3f70d1bd391d75
Matt <matthewky22@gmail.com>
2016-05-03 23:02:39 +0000

    End of day push

--- app/calendar/calendarC.js
+++ app/calendar/calendarC.js
@@ -12,45 +12,35 @@ function calendarC(Storage) {
         {
             breakfast: 'Cafe Mickey',
             lunch: 'Momma Melrose',
-            dinner: 'Tepan edo'
-        },
-        {
-            breakfast: 'Cafe Mickey2',
-            lunch: 'Momma Melrose2',
-            dinner: 'Tepan edo2'
-        },
-        {
-            breakfast: 'Cafe Mickey3',
-            lunch: 'Momma Melrose3',
-            dinner: 'Tepan edo3'
-        }    
+            dinner: 'Tepan edo',
+        }
     ];
     
-    self.add = function () {
-        self.cal.push({
-            <div class="week">
-                <div class="day"></div>
-                <div class="day"></div>
-                <div class="day"></div>
-                <div class="day"></div>
-                <div class="day"></div>
-                <div class="day"></div>
-                <div class="day"></div>
-            </div>
-        })
-    }
-    
+    // trying to add row for day.... throws my H3 tag  
+    // self.add = function() {
+    //     self.cal.push({
+    //         <div class="week">
+    //             <div class="day"></div>
+    //             <div class="day"></div>
+    //             <div class="day"></div>
+    //             <div class="day"></div>
+    //             <div class="day"></div>
+    //             <div class="day"></div>
+    //             <div class="day"></div>
+    //         </div>
+    //     })
+    // }
     
     // adds row object
-    self.add = function() {
-        self.cal.push({
-            breakfast: 'Hello',
-            lunch: 'world',
-            dinner: 'dinner'
-        });
-    };
+    // self.add = function() {
+    //     self.cal.push({
+    //         breakfast: 'Hello',
+    //         lunch: 'world',
+    //         dinner: 'dinner'
+    //     });
+    // };
     
-    // pushing keys to store in local storage
+     // pushing keys to store in local storage
     self.email = function() {
         Storage.set('calendar', self.cal);
     };
