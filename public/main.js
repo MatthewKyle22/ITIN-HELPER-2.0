451752d8f2a4cdf56482dd003aa5492518b04575
Matt Stevens <matthewky22@gmail.com>
2016-05-26 14:49:20 -0600

    bug fixes

--- public/main.js
+++ public/main.js
@@ -1,19 +1,26 @@
 angular.module('itinHelper', ['ui.router'])
     .config(Config);
-    
+
 //******Router Function*******
 function Config($stateProvider, $urlRouterProvider) {
     $stateProvider
         //add .states for each html page
+            .state('home', {
+                url: '/',
+                templateUrl: '/agentLogin/home.html'
+
+            })
+
             .state('calendar', {
                 url: '/calendar',
-                templateUrl: 'app/calendar/calendarV.html',
+                templateUrl: '/calendar/calendarV.html',
                 controller: 'calendarC as CalCtrl',
             })
             .state('clients', {
                 url: '/clients',
-                templateUrl: 'app/clients/clientsV.html'
+                templateUrl: '/clients/clientsV.html'
             });
 
-            $urlRouterProvider.otherwise('/calendar');
+
+            $urlRouterProvider.otherwise('/');
 }
--- server.js
+++ server.js
@@ -3,15 +3,15 @@ var express = require('express'),
     logger = require('morgan'),
     bodyParser= require('body-parser'),
     app = express(),
-    port = process.env.PORT || 2222
+    port = process.env.PORT || 3000
     // apiRoutes = require('./routes')
+
+mongoose.connect('mongodb://localhost:27017/calendar')
 
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({extended: true}))
     app.use(express.static(__dirname+'/public'))
     app.use(logger("dev"))
     // app.use('/api', apiRoutes)
-
-mongoose.connect('mongodb://itinhelper.net/calendar')
 
     app.listen(port)
